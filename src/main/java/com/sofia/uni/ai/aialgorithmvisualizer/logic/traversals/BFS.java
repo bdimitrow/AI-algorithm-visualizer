6cb4ed37a05e62da150290d86b1c6367ddf66691
Bozhidar Dimitrov <dbozhidar@vmware.com>
2023-02-08 21:39:50 +0200

    BFS returns LinkedList of states and queue data structure

--- src/main/java/com/sofia/uni/ai/aialgorithmvisualizer/logic/traversals/BFS.java
+++ src/main/java/com/sofia/uni/ai/aialgorithmvisualizer/logic/traversals/BFS.java
@@ -11,10 +11,11 @@ import java.util.*;
 
 public class BFS extends AbstractGraph {
     LinkedList<Map<Integer, LinkedList<Integer>>> graph;
-    Queue<State> states;
+    LinkedList<Map<State, Queue<Integer>>> linkedListStateQueue;
 
     public BFS(List<Integer> nodes, List<Pair<Integer, Integer>> edges) {
         super(nodes, edges);
+        this.linkedListStateQueue = new LinkedList<>();
     }
 
     public void traverse(int start) {
@@ -23,9 +24,11 @@ public class BFS extends AbstractGraph {
         for (Map<Integer, LinkedList<Integer>> map : graph) {
             int node = map.keySet().iterator().next();
             if (node == start) {
-                states.add(new State(node, NodeColor.RED));
                 queue.offer(node);
                 visited.add(node);
+                linkedListStateQueue.add(new HashMap<>() {{
+                    put(new State(node, NodeColor.RED), queue);
+                }});
             }
             while (!queue.isEmpty()) {
                 int current = queue.peek();
@@ -34,40 +37,29 @@ public class BFS extends AbstractGraph {
                         LinkedList<Integer> neighbors = m.get(current);
                         for (int neighbor : neighbors) {
                             if (!visited.contains(neighbor)) {
-                                states.offer(new State(neighbor, NodeColor.RED));
                                 queue.offer(neighbor);
                                 visited.add(neighbor);
+                                linkedListStateQueue.add(new HashMap<>() {{
+                                    put(new State(neighbor, NodeColor.RED), queue);
+                                }});
                             }
                         }
                     }
                 }
                 int removed = queue.poll();
-                states.offer(new State(removed, NodeColor.GREEN));
+                linkedListStateQueue.add(new HashMap<>() {{
+                    put(new State(removed, NodeColor.GREEN), queue);
+                }});
             }
         }
     }
 
-    public State getNextState() {
-        return this.states.poll();
+    public LinkedList<Map<State, Queue<Integer>>> getLinkedListStateQueue() {
+        return linkedListStateQueue;
     }
 
-//    public static void main(String[] args) {
-//        List<Integer> nodes = List.of(1,2,3,4,5,6,7,8,9,10);
-//        List<Pair<Integer,Integer>> edges = List.of(
-//                new Pair<>(1,2),
-//                new Pair<>(1,3),
-//                new Pair<>(1,4),
-//                new Pair<>(2,5),
-//                new Pair<>(2,6),
-//                new Pair<>(3,7),
-//                new Pair<>(3,8),
-//                new Pair<>(4,9),
-//                new Pair<>(5,10)
-//        );
-//        BFS bfsgraph = new BFS(nodes,edges);
-//        bfsgraph.traverse();
-//        State st = bfsgraph.getNextState();
-//        System.out.print(st.toString());
-//    }
-}
+    public Map<State, Queue<Integer>> getNextState(int index) {
+        return this.linkedListStateQueue.get(index);
+    }
 
+}
